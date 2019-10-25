b5a4e61cec59776f75ec6429d7250c7784d8a5e4
cxy1686901657 <1686901657@qq.com>
2019-10-25 17:19:02 +0800

    10.21

--- src/com/qc/leetcode/onebyone/DeleteNode.java
+++ src/com/qc/leetcode/onebyone/DeleteNode.java
@@ -49,4 +49,12 @@ public class DeleteNode {
       int val;
       ListNode next;
       ListNode(int x) { val = x; }
-  }
+
+     @Override
+     public String toString() {
+         return "ListNode{" +
+                 "val=" + val +
+                 ", next=" + next.val +
+                 '}';
+     }
+ }
