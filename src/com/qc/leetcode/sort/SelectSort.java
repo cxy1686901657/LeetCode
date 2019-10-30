5f19b04a163b0c6cc20c81cd84da800e3a86ea8d
cxy1686901657 <1686901657@qq.com>
2019-10-30 16:53:50 +0800

    选择排序

---
+++ src/com/qc/leetcode/sort/SelectSort.java
@@ -0,0 +1,30 @@
+package com.qc.leetcode.sort;
+
+/**
+ * @author qc
+ * @date 2019/10/30
+ * @description
+ * @project LeetCode
+ */
+
+public class SelectSort {
+    public static void main(String[] args){
+
+    }
+    public static void selectsort(int[] arr){
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for(int j=i+1;j<arr.length;j++){
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j; // 遍历 i+1~length 的值，找到其中最小值的位置
+                }
+            }
+            if (i != minIndex) {
+                temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
+}
