4c737d5f77bf896f93b9efc4bc7bb45541b10e33
cxy1686901657 <1686901657@qq.com>
2019-11-15 16:49:04 +0800

    插入排序

--- src/com/qc/leetcode/sort/InsertSort.java
+++ src/com/qc/leetcode/sort/InsertSort.java
@@ -9,34 +9,52 @@ package com.qc.leetcode.sort;
 
 public class InsertSort {
     public static void main(String[] args) {
-        int a = 20;
-        int b = 20;
-        Integer c = 200;
-        Integer d = 200;
-        int e = 200;
-        int f = 200;
-        Integer m = 20;
-        Integer n = 20;
-        System.out.println(a == b);
-        System.out.println(c == d);
-        System.out.println(e == f);
-        System.out.println(m == n);
+//        int a = 20;
+//        int b = 20;
+//        Integer c = 200;
+//        Integer d = 200;
+//        int e = 200;
+//        int f = 200;
+//        Integer m = 20;
+//        Integer n = 20;
+//        System.out.println(a == b);
+//        System.out.println(c == d);
+//        System.out.println(e == f);
+//        System.out.println(m == n);
+//        int i=0;
+        int arr[] = {2,1,3};
+        insertSort2(arr);
+//        insertSort(arr);
+        PrintUtil.print(arr);
     }
 
     public static void insertSort(int arr[]) {
-        for(int i=1;i<arr.length;i++){
-            int temp=arr[i];
-            int j=i-1;
-            for(;j>0;j--){
-                if(arr[j]<temp){
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            for (; j >= 0; j--) {
+                if (arr[j] < temp) {
                     break;
-                }else{
-                    arr[j+1]=arr[j];
+                } else {
+                    arr[j + 1] = arr[j];
                 }
             }
-            arr[j+1]=temp;
+            arr[j + 1] = temp;
         }
+    }
 
+    public static void insertSort2(int arr[]) {
+        int temp;
+        int leftIndex;
+        for (int i=1;i<arr.length;i++){
+            temp=arr[i];
+            leftIndex=i-1;
+            while (leftIndex>=0&& arr[leftIndex]>temp){
+                arr[leftIndex+1]=arr[leftIndex];
+                leftIndex--;
+            }
+            arr[leftIndex+1]=temp;
+        }
     }
 
 }
