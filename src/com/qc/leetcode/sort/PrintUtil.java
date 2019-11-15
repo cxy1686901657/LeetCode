5903f76d13c9ca41e36d22a7b678ac0df4b340ad
cxy1686901657 <1686901657@qq.com>
2019-11-15 16:49:45 +0800

    打印数组工具类

---
+++ src/com/qc/leetcode/sort/PrintUtil.java
@@ -0,0 +1,16 @@
+package com.qc.leetcode.sort;
+
+/**
+ * @author qc
+ * @date 2019/10/27
+ * @description
+ * @project LeetCode
+ */
+
+public class PrintUtil {
+    public static void print(int[] array){
+        for (int a :array){
+            System.out.printf("%d\t",a);
+        }
+    }
+}
