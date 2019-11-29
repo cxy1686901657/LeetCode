a0be3272bbe79cd3cfeec4f957af9e49352c5a1a
cxy1686901657 <1686901657@qq.com>
2019-11-29 12:06:38 +0800

    分割链表

---
+++ src/com/qc/leetcode/category/linkedlist/SplitListToParts.java
@@ -0,0 +1,38 @@
+package com.qc.leetcode.category.linkedlist;
+
+import com.qc.leetcode.entity.ListNode;
+
+/**
+ * @author qc
+ * @date 2019/11/28
+ * @description 分隔链表
+ * 题目描述：把链表分隔成 k 部分，每部分的长度都应该尽可能相同，排在前面的长度应该大于等于后面的。
+ * @project LeetCode
+ * @solving
+ */
+
+public class SplitListToParts {
+    public ListNode[] splitListToParts(ListNode root, int k) {
+        int N = 0;
+        ListNode cur = root;
+        while (cur != null) {
+            N++;
+            cur = cur.next;
+        }
+        int mod = N % k;
+        int size = N / k;
+        ListNode[] ret = new ListNode[k];
+        cur = root;
+        for (int i = 0; cur != null && i < k; i++) {
+            ret[i] = cur;
+            int curSize = size + (mod-- > 0 ? 1 : 0);
+            for (int j = 0; j < curSize - 1; j++) {
+                cur = cur.next;
+            }
+            ListNode next = cur.next;
+            cur.next = null;
+            cur = next;
+        }
+        return ret;
+    }
+}
